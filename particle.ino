db66bae11d43f816f3f5f2fc5c11d7bd2804348a
Andrew Burton <tuglyraisin@aol.com>
2015-12-12 20:49:53 -0600

    Updated numbers array to work with new number bit.
    Some of the values for the old number bits caused flashing. I fixed the
    first sixty to work better.

--- particle.ino
+++ particle.ino
@@ -9,7 +9,7 @@ unsigned long start;
 unsigned long lastSync;
 unsigned long oneHour = 60*60*1000;
 // Auto-set boolean
-bool autoset = true;
+int autoset = 1;
 
 // Write out a serial true or false signal
 void writeOut(int data) {
@@ -56,18 +56,20 @@ int setTime(String data_load) {
     String v = data_load.substring(1);
     // Once you manually set auto is tured off
     autoset = false;
+    // The value to return
+    int retval = -1;
     // Set the hour
     if (t.equals("h")) {
         setHour(v.toInt());
-        return 1;
+        retval = v.toInt();
     }
     // Set the minutes
     else if (t.equals("m")) {
         setMinute(v.toInt());
-        return 1;
+        retval = v.toInt();
     }
     // Unrecognized code
-    return -1;
+    return retval;
 }
 
 void manualSync() {
@@ -78,12 +80,12 @@ void manualSync() {
     setHour(Time.hour());
     setMinute(Time.minute());
     // Resets autoset to true
-    autoset = true;
+    autoset = 1;
 }
 
 void keepSync() {
+    if (autoset != 1) return;
     if (millis() > start) {
-        if (autoset != true) return; 
         if (millis() > lastSync+oneHour) {
             manualSync();
         }
@@ -95,16 +97,20 @@ void keepSync() {
 
 int setManual(String val) {
     if (val.equals("1")) {
-        autoset = true;
+        manualSync();
     } else {
-        autoset = false;
+        autoset = 0;
     }
+    return autoset;
 }
 
 void setup() {
     // Expose funtion
     Particle.function("settime", setTime);
     Particle.function("setmanual", setManual);
+    
+    // Expose variables
+    Particle.variable("autoset", &autoset, INT);
     
     // Setup the mode of the pins
     pinMode(pinSig, OUTPUT);
